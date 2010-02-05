c64d80b70fb3b51ace584049f33e7ce7330641a9
Alex Barth <ab@January.(none)>
2010-02-05 13:50:57 -0500

    Add install script.

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run install scripts for Atrium Tests and place results in Hudson $WORKSPACE.
+
+# Configure test
+# @todo Move to configuration file.
+PHPUNIT=/user/bin/phpunit
+SCRIPT=AtriumInstall.php
+
+# Create test directory for test files and run tests.
+TESTDIR=$WORKSPACE"/"$BUILD_ID"/results-install/"
+mkdir $TESTDIR
+$PHPUNIT --log-junit $TESTDIR"/install.log" $SCRIPT
